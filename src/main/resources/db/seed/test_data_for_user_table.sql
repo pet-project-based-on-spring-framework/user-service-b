f61cb8a679a8ea65227632414d2c048dec83ad75
Tsyupryk Roman <tsyupryk.roman@gmail.com>
2021-02-21 08:49:11 +0100

    refactor() adding more test data (#1)

--- src/main/resources/db/seed/test_data_for_user_table.sql
+++ src/main/resources/db/seed/test_data_for_user_table.sql
@@ -1,5 +1,13 @@
 INSERT INTO usr (first_name, last_name, username, email, password, birthday, created_date, updated_date)
-VALUES ('Roman', 'Tsyupryk', 'TRL', 'tsyupryk.roman@gmail.com', 'strong_password', '1988-06-26', '2021-01-01 8:30:00', '2021-01-01 11:30:00'),
-       ('first_name_test_1', 'last_name_test_1', 'username_test_1', 'test_email_1@mail.com', 'strong_password', '2000-01-01', '2021-01-02 8:30:00', '2021-01-02 11:30:00'),
-       ('first_name_test_2', 'last_name_test_2', 'username_test_2', 'test_email_2@mail.com', 'strong_password', '2000-01-02', '2021-01-03 8:30:00', '2021-01-03 11:30:00'),
-       ('first_name_test_3', 'last_name_test_3', 'username_test_3', 'test_email_3@mail.com', 'strong_password', '2000-01-03', '2021-01-04 8:30:00', '2021-01-04 11:30:00');
+VALUES ('testUserFirstName1', 'testUserLastName1', 'testUserUsername1', 'testUserEmail1@email.com', 'strong_password1', '2000-01-29', '2021-01-01 08:30:00', '2021-01-02 08:30:00'),
+       ('testUserFirstName2', 'testUserLastName2', 'testUserUsername2', 'testUserEmail2@email.com', 'strong_password2', '2000-01-28', '2021-01-03 08:30:00', '2021-01-04 08:30:00'),
+       ('testUserFirstName3', 'testUserLastName3', 'testUserUsername3', 'testUserEmail3@email.com', 'strong_password3', '2000-01-27', '2021-01-05 08:30:00', '2021-01-06 08:30:00'),
+       ('testUserFirstName4', 'testUserLastName4', 'testUserUsername4', 'testUserEmail4@email.com', 'strong_password4', '2000-01-26', '2021-01-07 08:30:00', '2021-01-08 08:30:00'),
+       ('testUserFirstName5', 'testUserLastName5', 'testUserUsername5', 'testUserEmail5@email.com', 'strong_password5', '2000-01-25', '2021-01-09 08:30:00', '2021-01-10 08:30:00'),
+       ('testUserFirstName6', 'testUserLastName6', 'testUserUsername6', 'testUserEmail6@email.com', 'strong_password6', '2000-01-24', '2021-01-11 08:30:00', '2021-01-12 08:30:00'),
+       ('testUserFirstName7', 'testUserLastName7', 'testUserUsername7', 'testUserEmail7@email.com', 'strong_password7', '2000-01-23', '2021-01-13 08:30:00', '2021-01-14 08:30:00'),
+       ('testUserFirstName8', 'testUserLastName8', 'testUserUsername8', 'testUserEmail8@email.com', 'strong_password8', '2000-01-22', '2021-01-15 08:30:00', '2021-01-16 08:30:00'),
+       ('testUserFirstName9', 'testUserLastName9', 'testUserUsername9', 'testUserEmail9@email.com', 'strong_password9', '2000-01-21', '2021-01-17 08:30:00', '2021-01-18 08:30:00'),
+       ('testUserFirstName10', 'testUserLastName10', 'testUserUsername10', 'testUserEmail10@email.com', 'strong_password10', '2000-01-20', '2021-01-19 08:30:00', '2021-01-20 08:30:00'),
+       ('testUserFirstName11', 'testUserLastName11', 'testUserUsername11', 'testUserEmail11@email.com', 'strong_password11', '2000-01-19', '2021-01-21 08:30:00', '2021-01-22 08:30:00'),
+       ('testUserFirstName12', 'testUserLastName12', 'testUserUsername12', 'testUserEmail12@email.com', 'strong_password12', '2000-01-18', '2021-01-23 08:30:00', '2021-01-24 08:30:00');
