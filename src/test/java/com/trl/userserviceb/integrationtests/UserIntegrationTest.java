5e965d8ea5f231994c55f0f29b5a9f8020e64e73
Tsyupryk Roman <tsyupryk.roman@gmail.com>
2021-02-21 09:05:04 +0100

    refactor() refactoring test UserIntegrationTest (#1)

--- src/test/java/com/trl/userserviceb/integrationtests/UserIntegrationTest.java
+++ src/test/java/com/trl/userserviceb/integrationtests/UserIntegrationTest.java
@@ -1,19 +1,29 @@
 package com.trl.userserviceb.integrationtests;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.trl.userserviceb.AbstractIntegrationTest;
+import com.trl.userserviceb.argumentprovider.UserArgumentsProvider;
+import com.trl.userserviceb.core.domain.User;
+import com.trl.userserviceb.core.model.UserDto;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
 import static com.trl.userserviceb.api.v1.controller.UserController.BASE_URL;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
@@ -22,17 +32,21 @@ class UserIntegrationTest extends AbstractIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper mapper;
+
     @Sql(value = {"/sqlscripts/User_Before.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     @Sql(value = {"/sqlscripts/User_After.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
-    @Test
-    void shouldReturnUserDto() throws Exception {
-        final long userId = 1L;
+    @ParameterizedTest
+    @ArgumentsSource(UserArgumentsProvider.class)
+    void shouldReturnUserDto(List<User> testDataUserList,
+                             List<UserDto> testDataUserDtoList) throws Exception {
 
-        final String expectedJsonResult =
-                "{\"id\":1,\"firstName\":\"Roman\",\"lastName\":\"Tsyupryk\",\"username\":\"TRL\","
-                        + "\"email\":\"tsyupryk.roman@gmail.com\",\"password\":\"strong_password\","
-                        + "\"birthday\":\"1988-06-26\",\"createdDate\":\"2021-01-01T08:30:00\","
-                        + "\"updatedDate\":\"2021-01-01T11:30:00\"}";
+        final UserDto expectedResponse = testDataUserDtoList.get(0);
+
+        final Long userId = expectedResponse.getId();
+
+        final String expectedJsonResult = mapper.writeValueAsString(expectedResponse);
 
         mockMvc.perform(get(BASE_URL + "/{userId}", userId))
                 .andExpect(status().isOk())
@@ -46,28 +60,28 @@ class UserIntegrationTest extends AbstractIntegrationTest {
     @Test
     void shouldReturnPageOfUserDto_defaultSizeOfPageIsTen() throws Exception {
 
-        final String expectedJsonResult =
-                "{\"id\":4,\"firstName\":\"first_name_test_3\",\"lastName\":\"last_name_test_3\","
-                        + "\"username\":\"username_test_3\",\"email\":\"test_email_3@mail.com\","
-                        + "\"password\":\"strong_password\",\"birthday\":\"2000-01-03\","
-                        + "\"createdDate\":\"2021-01-04T08:30:00\",\"updatedDate\":\"2021-01-04T11:30:00\"},"
-                        + "{\"id\":3,\"firstName\":\"first_name_test_2\",\"lastName\":\"last_name_test_2\","
-                        + "\"username\":\"username_test_2\",\"email\":\"test_email_2@mail.com\","
-                        + "\"password\":\"strong_password\",\"birthday\":\"2000-01-02\","
-                        + "\"createdDate\":\"2021-01-03T08:30:00\",\"updatedDate\":\"2021-01-03T11:30:00\"},"
-                        + "{\"id\":2,\"firstName\":\"first_name_test_1\",\"lastName\":\"last_name_test_1\","
-                        + "\"username\":\"username_test_1\",\"email\":\"test_email_1@mail.com\","
-                        + "\"password\":\"strong_password\",\"birthday\":\"2000-01-01\","
-                        + "\"createdDate\":\"2021-01-02T08:30:00\",\"updatedDate\":\"2021-01-02T11:30:00\"},"
-                        + "{\"id\":1,\"firstName\":\"Roman\",\"lastName\":\"Tsyupryk\",\"username\":\"TRL\","
-                        + "\"email\":\"tsyupryk.roman@gmail.com\",\"password\":\"strong_password\","
-                        + "\"birthday\":\"1988-06-26\",\"createdDate\":\"2021-01-01T08:30:00\","
-                        + "\"updatedDate\":\"2021-01-01T11:30:00\"}";
+        final int defaultSizeOfPage = 10;
 
         mockMvc.perform(get(BASE_URL))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(content().string(containsString(expectedJsonResult)))
+                .andExpect(jsonPath("$.pageable.pageSize", is(defaultSizeOfPage)))
+                .andExpect(jsonPath("$.numberOfElements", is(defaultSizeOfPage)))
+                .andDo(print());
+    }
+
+    @Sql(value = {"/sqlscripts/User_Before.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(value = {"/sqlscripts/User_After.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    @Test
+    void shouldReturnPageOfUserDto() throws Exception {
+
+        final int sizeOfPage = 8;
+
+        mockMvc.perform(get(BASE_URL + "?size=" + sizeOfPage))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.pageable.pageSize", is(sizeOfPage)))
+                .andExpect(jsonPath("$.numberOfElements", is(sizeOfPage)))
                 .andDo(print());
     }
 
